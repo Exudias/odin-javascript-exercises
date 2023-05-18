c79bb12b6920e92f44c2049e8321648a620af8ce
Exudias <editoo31@gmail.com>
2023-05-18 13:56:42 +0300

    Complete exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let oldest = undefined;
+    let oldestAge = 0;
+    for (person of people)
+    {
+        let deathYear = person.yearOfDeath || new Date().getFullYear();
+        let age = deathYear - person.yearOfBirth;
+        if (age > oldestAge)
+        {
+            oldestAge = age;
+            oldest = person;
+        }
+    }
+    return oldest;
 };
 
 // Do not edit below this line
