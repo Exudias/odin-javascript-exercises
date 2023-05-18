d59d400534b24f4af18a86f1e7ea227eb5f4f1c8
Exudias <editoo31@gmail.com>
2023-05-18 13:51:19 +0300

    Complete exercise 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,10 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    let result = [];
+    for (book of books)
+    {
+        result.push(book.title);
+    }
+    return result;
 };
 
 // Do not edit below this line
