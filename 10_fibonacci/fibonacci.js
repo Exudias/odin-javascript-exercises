134ef74b5350ab0fe806459830f998a1c0406b39
Exudias <editoo31@gmail.com>
2023-05-18 13:49:17 +0300

    Complete exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    n = +n; // convert to int from string
+    // validate against negatives
+    if (n < 0)
+    {
+        return "OOPS";
+    }
+    
+    if (n <= 2)
+    {
+        return 1;
+    }
+    else
+    {
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
 };
 
 // Do not edit below this line
