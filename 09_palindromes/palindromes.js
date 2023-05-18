147e8b79008612bc54bdb8bc91fb9d55a5ef06c5
Exudias <editoo31@gmail.com>
2023-05-18 13:44:24 +0300

    Complete exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    // first, standardize
+    let standardized = string.toLowerCase().replace(/[.,\/#!$%\^&\*;:{}=\-_`~() ]/g,"");
+    return standardized === standardized.split("").reverse().join("");
 };
 
 // Do not edit below this line
