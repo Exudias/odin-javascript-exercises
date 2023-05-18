20fe28c33b0caf1f5bf49a3fb758fe5847aa9c59
Exudias <editoo31@gmail.com>
2023-05-18 13:21:32 +0300

    Complete exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array) {
+    let newArray = [];
+    const argsArray = Array.from(arguments);
+    for (item of array)
+    {
+        if (argsArray.includes(item))
+            continue;
+        else
+            newArray.push(item);
+    }
+    return newArray;
 };
 
 // Do not edit below this line
