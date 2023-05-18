d09806624e9b2a63d7b1fd45e1e8469fd668ec48
Exudias <editoo31@gmail.com>
2023-05-18 13:27:20 +0300

    Complete exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(min, max) {
+    // validation
+    if (min < 0 || max < 0 || typeof min !== "number" || typeof max !== "number")
+    {
+        return "ERROR";
+    }
+    // switch around if larger number first
+    if (min > max)
+    {
+        let temp = min;
+        min = max;
+        max = temp;
+    }
 
+    let sum = 0;
+    for (let i = min; i <= max; i++)
+    {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
