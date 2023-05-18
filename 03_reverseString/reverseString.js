6ff526a336fa0fba67227e37f7fa67430a0c76f9
Exudias <editoo31@gmail.com>
2023-05-18 13:14:58 +0300

    Complete exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let output = "";
+    let stringArray = string.split("");
+    for (let i = stringArray.length - 1; i >= 0; i--)
+    {
+        output += stringArray[i];
+    }
+    return output;
 };
 
 // Do not edit below this line
